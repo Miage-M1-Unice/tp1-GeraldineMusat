4880a470aa523b51465c00ab0bd424eed53ba9e5
Geraldine MUSAT <geraldine.musat@hotmail.fr>
2019-02-19 11:05:16 +0100

    debut question 1b

---
+++ TP1/src/exercice1b/ParcoursFiltre.java
@@ -0,0 +1,21 @@
+package exercice1b;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+
+public class ParcoursFiltre extends SimpleFileVisitor<Path>{
+	
+	private Files f;
+	
+	public ParcoursFiltre() {
+		super();
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
